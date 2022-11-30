a7ca820b6e6fc9fdb2d6887b8eb600326b79b442
211110CHINTAN <211110@iiitt.ac.in>
2022-11-30 22:47:04 +0530

    Initial commit

---
+++ HospitalManagementSystem.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+
+struct patientdata
+{
+    int wardno, bedno;
+	string first_name;
+	string last_name;
+	int age;
+    long long ID;
+	char blood[100];
+	char gender;
+    patientdata*next;
+};
+class Hospital
+{
+	patientdata *head,*last;
+	public:
+	Hospital() //default constructor
+	{
+		head=NULL;
+		last=NULL;
+	}
+    patientdata input();
+    void insertatend();
+    void insertatbeg();
+    void getpatientoperate();
+    void getpatienttoout();
+    void listofpatients();
+    int search(int);
+    char departmentname[50];
+};
+
+patientdata Hospital :: input()
+{
+    int flag=0;
+   patientdata *p=new patientdata();
+   cout << "\n   Please enter data for patient\n";
+   cout<<"\n   First name     : ";
+   getline(cin,p->first_name);
+   cout << "   Last name      : ";
+   getline(cin,p->last_name);
+   again :
+   cout << "   Blood Group    : ";
+   cin>>p->blood;
+   if((strcmp(p->blood,"A+")==0)||(strcmp(p->blood,"a+")==0)||(strcmp(p->blood,"A-")==0)||(strcmp(p->blood,"a-")==0)||
+      (strcmp(p->blood,"B+")==0)||(strcmp(p->blood,"b+")==0)||(strcmp(p->blood,"B-")==0)||(strcmp(p->blood,"b-")==0)||
+      (strcmp(p->blood,"O+")==0)||(strcmp(p->blood,"o+")==0)||(strcmp(p->blood,"O-")==0)||(strcmp(p->blood,"o-")==0)||
+      (strcmp(p->blood,"AB+")==0)||(strcmp(p->blood,"ab+")==0)||(strcmp(p->blood,"AB-")==0)||(strcmp(p->blood,"ab-")==0))
+             flag=1;
+    if(flag==0)
+    {
+        cout<<"\n   Invalid Blood Group Try Again..\n\n";
+        goto again;
+
+    }
+   cout<<"   Gender(M/F)         :- ";
+   cin>>p->gender;
+   cout<<"   Age                 :- ";
+   cin>>p->age;
+   cout<<"   Mobile number       :- ";
+   cin>>p->ID;
+   cout<<" Enter the ward number :-";
+   cin>>p->wardno;
+   cout<<" Enter the bed number  :-";
+   cin>>p->bedno;
+
+
+	if(search(p->ID))
+	{
+		p->ID=0;
+		cout << "\n   Data not valid. Operation cancelled.";
+	}
+	return *p;
+}
